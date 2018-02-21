be2ee7fc3229094c1eef1fbe1c81ff63f97d1ecb
Alexandre Mclean <alexandre.mclean@gmail.com>
2018-02-21 16:38:15 -0500

    Separate pwsh extra modules in own script

--- scripts/k8s/enable-hyperv.ps1
+++ scripts/k8s/enable-hyperv.ps1
@@ -1,5 +1,1 @@
-# Load more modules in powershell core
-Install-Module WindowsPSModulePath -Force
-Add-WindowsPSModulePath
-
 Enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V -All -NoRestart
---
+++ scripts/k8s/pwsh-extra-modules.ps1
@@ -0,0 +1,3 @@
+# Load more modules in powershell core
+Install-Module WindowsPSModulePath -Force
+Add-WindowsPSModulePath
